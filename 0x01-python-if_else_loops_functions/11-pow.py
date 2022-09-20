f67f93834e6687fad65e3152268bbd5b034901cd
codeme254 <otwomadennis365@gmail.com>
2022-09-20 15:10:58 +0300

    task 11 correction

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b < 0:
-        result = 1
-        for i in range(0, b*-1):
-            result /= a
-    else:
-        result = 1
-        for i in range(0, b):
-            result *= a
-    return result
+    return a ** b
