7d3780981a4b533fd7bce418e0bd4e5f7edb6524
codeme254 <otwomadennis365@gmail.com>
2022-10-12 12:06:35 +0300

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """A blueprint for a square object"""
+    def __init__(self, size=0):
+        """size needs to be an integer and we need to check that"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
