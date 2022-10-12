2a777d49eefa0d824a0a84a4eabaed486535ee61
codeme254 <otwomadennis365@gmail.com>
2022-10-12 11:54:33 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+    """A class that defines a square with a private size variable"""
+
+    def __init__(self, size):
+        """the constructor for this square class"""
+        self.__size = size
