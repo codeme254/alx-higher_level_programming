1662f55addd0fd27d0b0c1bca29fadaf4223a84a
codeme254 <otwomadennis365@gmail.com>
2022-10-19 14:56:55 +0300

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,8 +4,8 @@
 
 
 class Rectangle:
-    number_of_instances = 0
     """Defines a rectangle"""
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         """Initializes a new Rectangle"""
         self.height = height
