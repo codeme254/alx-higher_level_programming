89de20a3cc69828e8f533f6f776b46c315d17f62
codeme254 <otwomadennis365@gmail.com>
2022-10-24 13:26:03 +0300

    final mandatory task

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""implementing a square class that inherits from a rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Blueprint for a square shape/figure"""
+    def __init__(self, size):
+        """initializing the properties of the square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """returns the area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """String representation of the class"""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__size) + "/" + str(self.__size)
+        return string
