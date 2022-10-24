6d1b4c89e0f9545161f5c8b570a1eac0303544b0
codeme254 <otwomadennis365@gmail.com>
2022-10-24 12:11:16 +0300

    task 0 requirement changing

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """returns the list of available attributes and methods of an object"""
+    """returns the list of available attributes and methods of an object
+    obj is the object to check"""
     return dir(obj)
