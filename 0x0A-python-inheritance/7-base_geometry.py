cc81dbc38b8880c2eb7aa8603bdbd94c729a9250
codeme254 <otwomadennis365@gmail.com>
2022-10-24 12:46:14 +0300

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a geometry module"""
+
+
+class BaseGeometry:
+    """Blueprint for the geometry"""
+
+    def area(self):
+        """supposed to find the area of something"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates name against value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
