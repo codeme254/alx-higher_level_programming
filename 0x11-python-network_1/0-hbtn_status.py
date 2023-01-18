e452647de73e9465b8b0a0556eeb68700eeafe1b
codeme254 <otwomadennis365@gmail.com>
2023-01-18 02:47:08 -0500

    what's my status 2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ Must use the with statement
 
 if __name__ == '__main__':
     import urllib.request
-    
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
